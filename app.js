d86b2f83f313586453961ca79b5de5cf37572368
Raul Menjivar <raul.menjivar4@gmail.com>
2021-05-25 10:09:43 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Now listening on Port 3000");
+});
